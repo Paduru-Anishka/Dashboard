7afcdfe7d5551bb1ac78cb8fe02c6d7dbd2b72ce
Paduru-Anishka <128974108+Paduru-Anishka@users.noreply.github.com>
2024-08-11 16:32:17 +0530

    Create  users.js

---
+++ src/assets/data/users.js
@@ -0,0 +1,10 @@
+export const users = [
+  {
+    id: 1,
+    username: 'user1',
+    name: 'User One',
+    email: 'user1@example.com',
+    activeLast24Hours: true,
+  },
+  // Add more dummy users here...
+];
